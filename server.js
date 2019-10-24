66408794fe5544cd596b9a3bbbca24ee25a5c3f0
Glitch (hello-express) <none>
2019-10-24 19:57:30 +0000

    🏗🐗 Checkpoint ./server.js:6661326/1872 ./package.json:6661326/1084 ./views/index.html:6661326/4417 ./public/style.css:6661326/1582

--- server.js
+++ server.js
@@ -2,21 +2,48 @@
 // where your node app starts
 
 // init project
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+
+app.get('/api/timestamp/:date_string?',(req, res) => {
+  let dateString = req.params.date_string;
+  let date;
+  
+  if(dateString == undefined){
+     date = new Date(); 
+  }else if ( (date = new Date(dateString)) == 'Invalid Date' ){
+     date = new Date(Number(dateString)); 
+  }
+  
+  if(date == 'Invalid Date'){
+    res.json({"error" : "Invalid Date"}); 
+  }
+  
+  let result = {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  };
+  
+  res.send(result);
+});
+
+
+
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
